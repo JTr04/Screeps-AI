fe887d713007054bdd815aca69e838c7e860c347
JTr04 <57480768+JTr04@users.noreply.github.com>
2020-12-27 23:12:38 +0800

    Create role.upgradera.js

---
+++ role.upgradera.js
@@ -0,0 +1,37 @@
+var roleUpgradera = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if(creep.memory.uping && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.uping = false;
+	    }
+	    if(!creep.memory.uping && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.uping = true;
+	        creep.say('UP');
+	    }
+	    if(creep.memory.uping) {
+	        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE
+    // 			||creep.signController(creep.room.controller,"逍遥半生酒中意，一剑碎影向征程") == ERR_NOT_IN_RANGE
+	        ) {
+                creep.moveTo(creep.room.controller,{visualizePathStyle: {stroke: '#ffaa00'}});
+            } 
+        }
+        else {
+            var sto = Game.getObjectById(Memory.creeps[creep.name].worksto)
+            if(creep.room.controller.level > 4 && sto.store.getUsedCapacity(RESOURCE_ENERGY) > 1000){
+                if(creep.withdraw(sto, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(sto);
+				}
+            }else{
+                var ids = Memory.creeps[creep.name].workloc;
+                var sourcea = Game.getObjectById(ids);
+                if(creep.harvest(sourcea) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sourcea, {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
+            }
+            
+        }
+	}
+};
+
+module.exports = roleUpgradera;
